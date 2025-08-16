568fad1fcb7975ea0d3d99744d1c907b8bc34dd0
Lantz-run <1196046661@qq.com>
2025-08-16 23:51:26 +0800

    违禁词拦截器和自定义模板

---
+++ src/main/java/com/lantz/lantzaiagent/advisor/ProhibitedAdvisor.java
@@ -0,0 +1,13 @@
+package com.lantz.lantzaiagent.advisor;
+
+/**
+ * <p>Project: lantz-ai-agent
+ * <p>Powered by Lantz On 2025/8/14
+ *
+ * @author Lantz
+ * @version 1.0
+ * @Description ProhibitedAdvisor
+ * @since 1.8
+ */
+public class ProhibitedAdvisor {
+}
---
+++ src/test/java/com/lantz/lantzaiagent/promptTemplate/testTemplate.java
@@ -0,0 +1,10 @@
+package com.lantz.lantzaiagent.promptTemplate;/**
+ * <p>Project: lantz-ai-agent 
+ * <p>Powered by Lantz On 2025/8/15
+ *
+ * @author Lantz
+ * @version 1.0
+ * @Description testTemplate
+ * @since 1.8
+ */public class testTemplate {
+}
